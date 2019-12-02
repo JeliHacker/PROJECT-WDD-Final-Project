87f86b000db4327e07e4fba48f2592a270e99611
JeliHacker <jeligooch@gmail.com>
2019-12-02 00:19:27 -0500

    added logos

---
+++ visualizationProject.js
@@ -0,0 +1,14 @@
+var screen = {width: 1000, height: 625}
+var margins = {top: 10, right: 50, bottom: 50, left:50}
+
+var svg = d3.select("svg");
+
+//svg.attr("width", width).attr("height", height)
+var completeData = [];
+ 
+d3.select(".dimensions").on("click", 
+        function()
+        {
+            svg.append("image").attr("href", "../nbaLogos/" + "BOS.svg").attr("width", "50px")
+        })
+
