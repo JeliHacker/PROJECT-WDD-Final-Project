a949f7f7a4ba08817d6f4fc2e849130f1cda92fc
JeliHacker <jeligooch@gmail.com>
2019-12-02 20:32:24 -0500

    changes

--- visualizationProject.js
+++ visualizationProject.js
@@ -6,7 +6,7 @@ var svg = d3.select("svg");
 //svg.attr("width", width).attr("height", height)
 var completeData = [];
  
-d3.select(".dimensions").on("click", 
+d3.select(".container").on("click", 
         function()
         {
             svg.append("image").attr("href", "../nbaLogos/" + "BOS.svg").attr("width", "50px")
